39ad9aff53494ba20fa4b187f56f937dfc596854
Joshua Tighe <jtighe3@nd.edu>
2025-10-07 14:45:44 -0400

    task manager heading

--- src/app/(homepage)/task-manager/page.tsx
+++ src/app/(homepage)/task-manager/page.tsx
@@ -1,5 +1,22 @@
+import { LucidePlus } from "lucide-react";
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+
 const TaskManager = () => {
-  return <div>task manager</div>;
+  return (
+    <div>
+      <div className="flex flex-1 justify-between items-center">
+        <Heading
+          title="Task Manager"
+          description="Create and manage your projects and hourly rates"
+        />
+        <Button className="mr-8">
+          <LucidePlus />
+          Add Task
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default TaskManager;
--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -5,7 +5,7 @@ type HeadingProps = {
 
 const Heading = ({ title, description }: HeadingProps) => {
   return (
-    <div className="px-8">
+    <div className="ml-8">
       <h2 className="text-2xl font-semibold tracking-tight">{title}</h2>
       {description && <p className="text-sm text-gray-500">{description}</p>}
     </div>
