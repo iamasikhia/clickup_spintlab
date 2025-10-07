af2b473eb02c24672efcf75be4ace8ca9fad3be6
Joshua Tighe <jtighe3@nd.edu>
2025-10-07 15:05:55 -0400

    extract task card

--- src/app/(homepage)/task-manager/page.tsx
+++ src/app/(homepage)/task-manager/page.tsx
@@ -1,19 +1,7 @@
-import {
-  LucideDollarSign,
-  LucidePen,
-  LucidePlus,
-  LucideTrash,
-} from "lucide-react";
+import { LucidePlus } from "lucide-react";
 import { Heading } from "@/components/heading";
-import { Badge } from "@/components/ui/badge";
+import { TaskCard } from "@/components/task-card";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 const TaskManager = () => {
   return (
@@ -29,30 +17,13 @@ const TaskManager = () => {
         </Button>
       </div>
       <div className="mx-8">
-        <Card className="max-w-[420px]">
-          <CardHeader className="flex justify-between items-center">
-            <div>
-              <CardTitle>Task Name</CardTitle>
-              <CardDescription>Details describing the task</CardDescription>
-            </div>
-            <Badge>active</Badge>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-y-4">
-            <div className="flex flex-row gap-x-4 items-center">
-              <LucideDollarSign width="16" height="16" color="#717182" />
-              <span className="font-semibold">75.00/hour</span>
-            </div>
-            <div className="text-gray-500 text-sm">Created: 1/14/2004</div>
-            <div className="flex flex-row gap-x-2">
-              <Button variant="outline" size="sm">
-                <LucidePen />
-              </Button>
-              <Button variant="outline" size="sm">
-                <LucideTrash />
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
+        <TaskCard
+          title="Task Card"
+          description="Description of the card"
+          status="active"
+          rate="75.00"
+          created="1/4/2014"
+        />
       </div>
     </div>
   );
