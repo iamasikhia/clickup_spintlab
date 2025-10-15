825a4119c02f7758de3dc30a741431a5190303fe
Kristofer Ulanday <kulanday@nd.edu>
2025-10-15 19:26:59 -0400

    clickup-api.py

---
+++ backend/app/service-apis/clickup-api.py
@@ -0,0 +1,12 @@
+client_id = ""
+redirect_uri = ""
+state = ""
+secret = "" # or client_secret, DO NOT ADD!
+code = ""
+
+# send users to this URL https://app.clickup.com/api?client_id={client_id}&redirect_uri={redirect_uri}
+# can add &state={state}
+
+
+# step 3: request token
+# get access token endpoint
