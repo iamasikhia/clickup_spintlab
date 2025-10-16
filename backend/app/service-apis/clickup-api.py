da6adcbcc29e86474cdfeea300079d3acb6a86f1
Kristofer Ulanday <kulanday@nd.edu>
2025-10-15 22:13:23 -0400

    adding clickup required data

--- backend/app/service-apis/clickup-api.py
+++ backend/app/service-apis/clickup-api.py
@@ -7,6 +7,23 @@ code = ""
 # send users to this URL https://app.clickup.com/api?client_id={client_id}&redirect_uri={redirect_uri}
 # can add &state={state}
 
-
 # step 3: request token
 # get access token endpoint
+# queries send to 
+
+data = {
+
+
+}
+
+# connect DB and clickup
+# get data from clickup THROUGH REST API
+# align josh to get all data from frontend
+# rest apis allow us to comm. to backend
+
+# page.tsx 
+# collecting showPassword and setEmail, etc etc as email and password
+# /signup and /signin api
+# models.py, schemas.py
+# user(base) with user and password
+# all we need are the tasks
