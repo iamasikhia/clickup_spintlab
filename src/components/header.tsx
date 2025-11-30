f62192fdcbad2515cf1f2324462c4243dcdcae5a
Joshua Tighe <jtighe3@nd.edu>
2025-11-30 13:03:00 -0600

    redirect to login page on clicking sign out (temp)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { LucideLogOut } from "lucide-react";
+import Link from "next/link";
 import { ThemeSwitcher } from "./theme/theme-switcher";
 import { Button } from "./ui/button";
 
@@ -13,9 +14,11 @@ const Header = ({ page, username }: HeaderProps) => {
       <div className="text-lg font-semibold">{page}</div>
       <div className="flex gap-x-4 items-center">
         <div className="text-sm">Welcome, {username}</div>
-        <Button variant="destructive">
-          <LucideLogOut />
-          <span className="font-semibold">Sign Out</span>
+        <Button variant="destructive" asChild>
+          <Link href="/">
+            <LucideLogOut />
+            <span className="font-semibold">Sign Out</span>
+          </Link>
         </Button>
         <ThemeSwitcher />
       </div>
